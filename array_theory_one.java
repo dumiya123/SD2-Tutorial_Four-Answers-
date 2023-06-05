425dc7fc0e234f64972278fd5284668731d866de
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-06-05 15:46:00 +0530

    initial commit

--- array_theory_one.java
+++ array_theory_one.java
@@ -3,7 +3,11 @@ public class array_theory_one
     public static void main(String[] args)
     {
         //lets declare a array
-        
+        int[] array ={1,2,3,4,5,6};
+        System.out.println(array.length);
+        int[] numbers;
+        numbers=new int[]
+
 
 
         
