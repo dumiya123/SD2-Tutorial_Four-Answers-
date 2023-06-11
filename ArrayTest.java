97da27eac7deedc31badb39df40e6eb17e2a21e0
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-06-11 08:04:37 +0530

    initial commit

--- ArrayTest.java
+++ ArrayTest.java
@@ -8,26 +8,44 @@ public class ArrayTest
         System.out.println("Enter number of students: ");
         int num_of_students = scanner.nextInt();
         scanner.nextLine();
-        double[] scores_of_students =new double[num_of_students];
-        String[] students_names = new String[num_of_students];
+        double[] scores_of_students =new double[num_of_students];   //declare this array to hold scores
+        String[] students_names = new String[num_of_students];      //declare this array to hold names of students.
         double totalScore = 0.0;
 
-        for(int i = 0; i < num_of_students; i++)
+        for(int i = 0; i < num_of_students; i++)                        //create a for loop for get information from teacher.
         {
 
-            System.out.println("Enter name of the student: ");
-            students_names[i] = scanner.nextLine();
+            System.out.println("Enter name of the student: ");       //get the name 
+            students_names[i] = scanner.nextLine();                   //store it in the array called students_names.
             System.out.println("Enter the score for "+students_names[i]+" : ");
             scores_of_students[i]=scanner.nextDouble();
+            scanner.nextLine();
 
-            totalScore += scores_of_students[i];
+            totalScore += scores_of_students[i];   //calculate total score  of the students
 
 
         }
 
         //calculate the average score students.
 
-        double averageScore =
+        double averageScore =totalScore/num_of_students;
+
+        System.out.println("\nTest Analysis");
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("Name\tScore\tDeviation");
+        System.out.println("---------------------------------------------------------------------------------");
+
+
+        for(int i=0; i<num_of_students;i++)
+        {
+            double deviation=scores_of_students[i]-averageScore;
+            System.out.printf( "%-15s\t%.2f\t%.2f%n",students_names[i], " : ",scores_of_students[i],deviation);
+        }
+
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.printf("Average score:\t%.2f%n", averageScore);
+        System.out.println("---------------------------------------------------------------------------------");
+        scanner.close();
 
 
 
---
+++ array_theory.java
@@ -0,0 +1,12 @@
+public class array_theory 
+{
+    public static void main(String[] args) 
+    {
+        
+          System.out.println("Name:\nScore:\nDeviation:\n");
+        
+    }
+    
+
+    
+}
