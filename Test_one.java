bc132f48283be3b7424649af7283e6e417d43d6b
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-06-18 22:06:51 +0530

    initial commit

---
+++ Test_one.java
@@ -0,0 +1,21 @@
+public class Test_one
+{
+    public static void main(String[] args)
+    {
+        
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int element : array)
+        {
+
+           System.out.println(array[element]);
+            
+           
+        }
+
+        
+
+        
+    }
+    
+}
---
+++ two_dimesional_array_one.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class two_dimesional_array_one 
+{
+    public static void main(String[] args) 
+    {
+        int[][] array=new int[3][];
+
+        array[0]=new int[3];
+        array[1]=new int[5];
+        array[2]=new int[7];
+
+        Scanner scanner=new Scanner(System.in);
+
+        
+
+
+
+
+    }
+
+
+
+        
+
+
+
+
+       
+
+        
+    }
+    
+}
