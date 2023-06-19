d923059984eda85b10accd40a61ab5b57cbfbe5b
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-06-19 12:46:29 +0530

    initial commit

--- two_dimesional_array_one.java
+++ two_dimesional_array_one.java
@@ -1,18 +1,26 @@
-import java.util.Scanner;
 
 public class two_dimesional_array_one 
 {
     public static void main(String[] args) 
     {
-        int[][] array=new int[3][];
+        int[][] array={{1,2,3,4},{1,2,3,4}};
 
-        array[0]=new int[3];
-        array[1]=new int[5];
-        array[2]=new int[7];
+        for (int i=0; i<array.length; i++) 
+        {
+            for (int j = 0; j < array[i].length; j++) 
+            {
+                System.out.println(array[i][j]);
+                
+            }
+            
+        }
 
-        Scanner scanner=new Scanner(System.in);
 
-        
+       
+
+
+
+
 
 
 
@@ -31,4 +39,5 @@ public class two_dimesional_array_one
         
     }
     
-}
+
+
